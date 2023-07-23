6e810865d236db752e51ecec3595ffa3bb9eda03
Alnnu <luannf1020@gmail.com>
2023-07-22 23:37:02 -0300

    add: componente Sobre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/home/Hero";
+import Sobre from "@/components/home/Sobre";
 
 export default function Home() {
   return (
       <>
-        <Hero/>
+          <Hero/>
+          <Sobre/>
       </>
   )
 }
---
+++ src/components/home/Sobre.tsx
@@ -0,0 +1,17 @@
+export default function Sobre() {
+    return (
+        <div>
+            <div className="flex justify-center">
+                <div className="avatar">
+                    <div className="w-32 mask mask-hexagon-2 ">
+                        <img src="foto-palestra.jpeg" />
+                    </div>
+                </div>
+            </div>
+            <div className="text-center">
+                <h2 className="text-3xl font-bold mt-4">Luann Felipe</h2>
+                <p>Olá👋, me chamo Luann Felipe, mas pode me chamar apenas de Alnnu, tenho 20 anos.<br/> Atualmente estou cursando o 6 período de ciência da computação no IFG Anápolis</p>
+            </div>
+        </div>
+    )
+}
