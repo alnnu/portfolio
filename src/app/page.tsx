ed9d7f13c1d6db1fc25d37d3007ce66ffc871bf9
Alnnu <luannf1020@gmail.com>
2023-07-24 16:59:30 -0300

    add:componente MinhasTech

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/Hero";
 import Sobre from "@/components/home/Sobre";
+import MinhasTech from "@/components/home/MinhasTech";
 
 export default function Home() {
   return (
       <>
           <Hero/>
           <Sobre/>
+          <MinhasTech/>
       </>
   )
 }
---
+++ src/components/home/TechCard.tsx
@@ -0,0 +1,19 @@
+// @ts-ignore
+export default function TechCard({ techName, techSvg }) {
+    return (
+        <div>
+            <div className="card w-32">
+                <figure>
+                    <svg viewBox="0 0 128 128" height="50" width="50">
+                        <path fillRule="evenodd" clipRule="evenodd"  d={techSvg} fill="#4D4D4D"></path>
+                    </svg>
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{techName}</h2>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
