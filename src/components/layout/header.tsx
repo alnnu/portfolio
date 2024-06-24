69897121daf1cec67dab19ab5e1151959ab68e3d
alnnu <luannf1020@gmail.com>
2024-06-24 11:41:45 -0300

    first commit

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,26 @@
+import { BiMessageSquareDots } from "react-icons/bi";
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="flex py-5">
+                    <div className="font-bold flex-1 text-lg">
+                        <a href="#">Alnnu</a>
+                    </div>
+                    <div className="flex flex-1 justify-center text-lg">
+                        <a href="#" className="mx-5 text-primery">Home</a>
+                        <a href="#" className="mx-5 hover:text-primery">Sobre</a>
+                        <a href="#" className="mx-5 hover:text-primery">Experiencias</a>
+                        <a href="#" className="mx-5 hover:text-primery" >Projetos</a>
+                    </div>
+                    <div className="flex flex-1 justify-end text-2xl    ">
+                        <BiMessageSquareDots />
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
