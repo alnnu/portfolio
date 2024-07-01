64cc1f585cc27d446ad6f1419b5ba8a6e6e68a47
alnnu <luannf1020@gmail.com>
2024-07-01 10:59:37 -0300

    .

--- src/components/habilidadesCard.tsx
+++ src/components/habilidadesCard.tsx
@@ -2,11 +2,11 @@ const Habilidades = ({nome, techs}: {nome: String, techs: String[]}) => {
     return (
         <>
             <div>
-                <h2 className="text-center text-2xl font-bold mb-5">{nome}</h2>
-                <div className="grid grid-cols-4 gap-4">
+                <h2 className="text-center text-2xl font-bold mb-5 ">{nome}</h2>
+                <div className="flex flex-wrap auo-cols-auto  justify-center items-center gap-4">
                     {
                         techs.map((tech: any, index: number) => (
-                            <div className="text-center text-white py-1 rounded-full bg-primery">{tech}</div>
+                            <div className="text-center text-white  py-1 w-36 h-9 rounded-full bg-primery">{tech}</div>
                         ))
                     }
                 </div>
